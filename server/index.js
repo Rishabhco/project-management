2620e1d99fc5b228e38f37deef5b92142ded34b9
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-08-10 16:26:04 +0530

    added db config

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express=require('express');
+const colors=require('colors');
 require('dotenv').config();
 const {graphqlHTTP}=require('express-graphql');
 const schema =require('./schema/schema');
+const connectDb=require('./config/db');
 const port =process.env.PORT || 3000;
 const app=express();
+
+connectDb();
 
 app.use('/graphql',graphqlHTTP({
     schema,
