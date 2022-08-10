0f8909c584d01a8c58f5e4c3385b680f20c4cc66
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-08-10 15:59:06 +0530

    added projects schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,11 +2,23 @@ const {projects,clients}=require('../sampleData');
 
 const {GraphQLObjectType,GraphQLID,GraphQLString,GraphQLList,GraphQLSchema}=require('graphql');
 
+const ProjectType=new GraphQLObjectType({
+    name:"Project",
+    fields:{
+        id:{type:GraphQLID},
+        clientId:{type:GraphQLID},
+        name:{type:GraphQLString},
+        description:{type:GraphQLString},
+        status:{type:GraphQLString}
+    }
+})
+
 const ClientType=new GraphQLObjectType({
     name:"Client",
     fields:{
         id:{type:GraphQLID},
         name:{type:GraphQLString},
+        email:{type:GraphQLString},
         phone:{type:GraphQLString}
     }
 })
@@ -14,8 +26,24 @@ const ClientType=new GraphQLObjectType({
 const RootQuery=new GraphQLObjectType({
     name:"RootQueryType",
     fields:{
+        projects:{
+            type:new GraphQLList(ClientType),
+            resolve(parent,args){
+                return projects;
+            }
+        },
+        project:{
+            type:ClientType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return projects.find(project=>project.id===args.id);
+            }
+        },
         clients:{
             type:new GraphQLList(ClientType),
+            resolve(parent,args){
+                return clients;
+            }
         },
         client:{
             type:ClientType,
