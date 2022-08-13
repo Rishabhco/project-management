c5a5d7ed2f0b593bcf49904e841c6094dd893806
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-08-13 23:36:29 +0530

    added mutation for clients

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,7 +3,7 @@
 const Project=require('../models/Project');
 const Client=require('../models/Client');
 
-const {GraphQLObjectType,GraphQLID,GraphQLString,GraphQLList,GraphQLSchema}=require('graphql');
+const {GraphQLObjectType,GraphQLNonNull,GraphQLEnumType,GraphQLID,GraphQLString,GraphQLList,GraphQLSchema}=require('graphql');
 
 const ClientType=new GraphQLObjectType({
     name:"Client",
@@ -64,6 +64,38 @@ const RootQuery=new GraphQLObjectType({
     }
 })
 
+const mutation=new GraphQLObjectType({
+    name:"Mutation",
+    fields:{
+        addClient:{
+            type:ClientType,
+            args:{
+                name:{type:GraphQLNonNull(GraphQLString)},
+                email:{type:GraphQLNonNull(GraphQLString)},
+                phone:{type:GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parent,args){
+                let client=new Client({
+                    name:args.name,
+                    email:args.email,
+                    phone:args.phone
+                });
+                return client.save();
+            }
+        },
+        deleteClient:{
+            type:ClientType,
+            args:{
+                id:{type:GraphQLNonNull(GraphQLID)},
+            },
+            resolve(parent,args){
+                return Client.findByIdAndRemove(args.id);
+            }
+        },
+    }
+})
+
 module.exports=new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery,
+    mutation
 })
