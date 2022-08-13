125e0959567a7eea245259ca8c2f3512be820750
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-08-13 23:41:56 +0530

    added mutations for project

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -92,6 +92,70 @@ const mutation=new GraphQLObjectType({
                 return Client.findByIdAndRemove(args.id);
             }
         },
+        addProjects:{
+            type:ProjectType,
+            args:{
+                name:{type:GraphQLNonNull(GraphQLString)},
+                description:{type:GraphQLNonNull(GraphQLString)},
+                status:{
+                    type:new GraphQLEnumType({
+                        name:"ProjectStatus",
+                        values:{
+                           'new':{value:'Not Started'},
+                           'progress':{value:'In Progress'},
+                           'completed':{value:'Completed'},
+                        }
+                    }),
+                },
+                defaultValue:'Not Started',
+                clientId:{type:GraphQLNonNull(GraphQLID)},
+            },
+            resolve(parent,args){
+                let project=new Project({
+                    name:args.name,
+                    description:args.description,
+                    status:args.status,
+                    clientId:args.clientId
+                });
+                return project.save();
+            },
+        },
+        deletePorject:{
+            type:ProjectType,
+            args:{
+                id:{type:GraphQLNonNull(GraphQLID)},
+            },
+            resolve(parent,args){
+                return Project.findByIdAndRemove(args.id);
+            }
+        },
+        updateProject:{
+            type:ProjectType,
+            args:{
+                id:{type:GraphQLNonNull(GraphQLID)},
+                name:{type:GraphQLString},
+                description:{type:GraphQLString},
+                status:{
+                    type:new GraphQLEnumType({
+                        name:"StausUpdate",
+                        values:{
+                           'new':{value:'Not Started'},
+                           'progress':{value:'In Progress'},
+                           'completed':{value:'Completed'},
+                        }
+                    }),
+                },
+            },
+            resolve(parent,args){
+                return Project.findByIdAndUpdate(args.id,{
+                    $set:{
+                        name:args.name,
+                        description:args.description,
+                        status:args.status
+                    }
+                },{new:true});
+            }
+        }
     }
 })
 
