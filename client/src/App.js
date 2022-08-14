03153d70fcb1869b5779d831f8c043e35e209298
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-08-15 00:49:10 +0530

    ✨added Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import Header from "./components/Header";
+import {ApolloProvider,ApolloClient,InMemoryCache} from '@apollo/client';
+
+const client=new ApolloClient({
+  uri:'http://locahost:5000/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <ApolloProvider client={client}>
+      <Header/>
+      <div className="container">
+        <h1>Hello World</h1>
+      </div>
+    </ApolloProvider>
+    </>
   );
 }
 
