3bd2ace80aa38bdafb01ccc543b64c4c0c404055
anushkapatil18 <anushkabpatil11@gmail.com>
2022-08-16 02:12:38 +0530

    frontend complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
+import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
 import {ApolloProvider,ApolloClient,InMemoryCache} from '@apollo/client';
 import Header from "./components/Header";
-import Clients from "./components/Clients";
+import Home from './pages/Home';
+import Project from './pages/Project';
+import NotFound from './pages/NotFound';
+import Clients from './components/Clients';
+import AddClientModal from './components/AddClientModal';
+import Projects from './components/Projects';
+
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query:{
+      fields:{
+        clients: {
+          merge(existing, incoming){
+            return incoming;
+          }
+        },
+        projects:{
+          merge(existing, incoming){
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+})
 
 const client=new ApolloClient({
-  uri:'http://locahost:5000/graphql',
-  cache: new InMemoryCache(),
+  uri:'http://locahost:3001/graphql',
+  cache,
 })
 
 function App() {
@@ -13,7 +39,13 @@ function App() {
     <ApolloProvider client={client}>
       <Header/>
       <div className="container">
-        <Clients/>
+       <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/projects/:id' element={<Project />} />
+          <Route path="*" element={<NotFound />} />
+          </Routes>
+  </Router>
       </div>
     </ApolloProvider>
     </>
