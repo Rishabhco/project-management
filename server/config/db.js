3ede79501cef2807bc1882948ac7483fa5ebe550
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-08-19 13:17:46 +0530

    updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,12 @@
 const mongoose=require("mongoose");
 
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if(!MONGO_URI){
+    throw new Error("You must provide a MongoLab URI");
+}
+
 const connectDb=async()=>{
     const conn=await mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser:true,
