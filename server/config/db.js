578037aeeef5ad6078580666eb849ab47f03fea4
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-08-19 13:26:26 +0530

    updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,18 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
 
 const MONGO_URI = process.env.MONGO_URI;
 
-if(!MONGO_URI){
+if (!MONGO_URI) {
     throw new Error("You must provide a MongoLab URI");
 }
 
-const connectDb=async()=>{
-    const conn=await mongoose.connect(process.env.MONGO_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
+const connectDb = async () => {
+    const conn = await mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);      
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 }
 
-module.exports=connectDb;
+module.exports = connectDb;
